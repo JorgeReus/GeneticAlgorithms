c1a7611969a1f9c7140778db4c8d83053c63e1b5
Jorge Reus <j-g1996@live.com>
2018-08-30 22:37:58 -0500

    Up to date

--- pracs/p1/historigram/historigram.cpp
+++ pracs/p1/historigram/historigram.cpp
@@ -5,35 +5,59 @@
 #include <vector>
 #include <bits/stdc++.h>
 
-const int centerX = 100; 
-const int centerY = 100; 
+// Window Size
+const int windowH = 750;
+const int windowW = 1420;
+// Plot Size
+const int plotPadding = 70;
+const int plotH = windowH - plotPadding;
+const int plotW = windowW - plotPadding;
+const int plotCx = plotW/2;
+const int plotCy = plotH/2;
+// Axis Position
+const int x = plotH - plotPadding;
 
 using namespace std;
-void drawFrame() {
-	 	gfx_line(centerX, centerY, 0, 0);
+void drawPlotFrame() {
+	gfx_color(9, 71, 122);
+	gfx_point(plotCx, plotCy);
+	// X-Axis
+	gfx_line(plotPadding, x, plotW - plotPadding, x);
+	// Y-Axis
+	gfx_line(plotPadding*2, plotPadding, plotPadding*2, plotH);
+	gfx_color(0, 200, 100);
 }
 
 void printHistogram(int arr[], int n)
 {
-
-	gfx_open(800, 600, "Historigrama");
+	gfx_open(windowW, windowH, "Historigrama");
 	gfx_color(0, 200, 100);
- 
- 	int height = 500;
- 	int width = 20;
- 	int delta = 200;
+	int barPadding = (plotW - plotPadding*3)/n;
+ 	int delta = barPadding + plotPadding*2;
+	int maxElement = *max_element(arr, arr + n);
     while(1) {
     	gfx_clear();
- 		//drawFrame();
+		drawPlotFrame();
  		char str[12] = {'\0'};
-    	for(int i=0, j=0; i < n; i++)
+    	for(int i=0, barPos=delta; i < n; i++)
  		{
 			sprintf(str, "%d", arr[i]);
- 			gfx_line(delta + j, height, delta + j, delta - arr[i]*10);
- 			gfx_text( delta + j, delta - arr[i]*10 - 10 , str);
+			if (maxElement == arr[i]) {
+ 				gfx_line(barPos, x, barPos, plotPadding);
+				gfx_color(9, 71, 122);
+ 				gfx_text(barPos, plotPadding - 10, str);
+				gfx_color(0, 200, 100);
+			} else {
+				unsigned long long yBar = x - 
+				(((double)arr[i] * (plotH - 2*plotPadding) / (double)maxElement));
+				gfx_line(barPos, x, barPos, yBar);
+				gfx_color(9, 71, 122);
+				gfx_text(barPos, yBar - 10, str);
+				gfx_color(0, 200, 100);
+			}
  			sprintf(str, "%d", i + 1);
- 			gfx_text( delta + j, height + 20 , str);
- 			j+=20;
+ 			gfx_text(barPos, plotH-plotPadding + 20 , str);
+ 			barPos+=barPadding;
  		}
  		if(gfx_event_waiting()){
  			if(gfx_wait() == 'q'){
@@ -47,10 +71,15 @@ void printHistogram(int arr[], int n)
 
 int main(int argc, char *argv[])
 {
+	int n = 10;
+	if(argc == 2){
+		n = atoi(argv[1]);
+	}
 	srand(time(0));
-	int arr[10] = { 10, 9, 12, 4, 5, 2,
-                    8, 5, 3, 1 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+	int arr[n];
+	for(int i=0; i < n; i++) {
+		arr[i] = rand() % (100 + 1) + 1;
+	}
     printHistogram(arr, n);
     return 0;
 }
